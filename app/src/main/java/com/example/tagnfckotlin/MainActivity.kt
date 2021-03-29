85dd867b515cc4a8989d5e3e9298ad2f16e9447b
matteobudai <69252141+matteobudai@users.noreply.github.com>
2021-03-29 15:40:34 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/tagnfckotlin/MainActivity.kt
+++ app/src/main/java/com/example/tagnfckotlin/MainActivity.kt
@@ -158,9 +158,9 @@ class MainActivity : AppCompatActivity() {
     private fun toHex(bytes: ByteArray): String {
         val sb = StringBuilder()
         for (i in bytes.indices.reversed()) {
-            val b: Byte = bytes[i] and 0xff.toByte()
+            val b: Int = bytes[i].toInt() and 0xff
             if (b < 0x10) sb.append('0')
-            sb.append(Integer.toHexString(b.toInt()))
+            sb.append(Integer.toHexString(b))
             if (i > 0) {
                 sb.append(" ")
             }
@@ -174,9 +174,9 @@ class MainActivity : AppCompatActivity() {
             if (i > 0) {
                 sb.append(" ")
             }
-            val b: Byte = bytes[i] and 0xff.toByte()
+            val b: Int = bytes[i].toInt() and 0xff
             if (b < 0x10) sb.append('0')
-            sb.append(Integer.toHexString(b.toInt()))
+            sb.append(Integer.toHexString(b))
         }
         return sb.toString()
     }
