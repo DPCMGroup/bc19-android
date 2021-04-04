cc29adf06250007ffcd8ab7cc348b0f2f3308a3a
matteobudai <tisco99@hotmail.it>
2021-04-04 20:50:17 +0200

    Sistemazione prenotazione

--- app/src/main/java/com/example/tagnfckotlin/PrenotaActivity.kt
+++ app/src/main/java/com/example/tagnfckotlin/PrenotaActivity.kt
@@ -3,6 +3,7 @@ package com.example.tagnfckotlin
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Intent
+import android.icu.util.Calendar
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
@@ -12,6 +13,7 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.Calendar.*
 
 class PrenotaActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
@@ -27,17 +29,17 @@ class PrenotaActivity : AppCompatActivity() {
         val SelezionaData = findViewById<ImageButton>(R.id.SelezionaData)
         val dataTesto = findViewById<TextView>(R.id.dataTesto)
 
-        val now = Calendar.getInstance()
-        var day = now.get(Calendar.DAY_OF_MONTH)
+        val now = getInstance()
+        var day = now.get(DAY_OF_MONTH)
         var month = now.get(Calendar.MONTH)
-        var year = now.get(Calendar.YEAR)
+        var year = now.get(YEAR)
 
         SelezionaData.setOnClickListener {
 
             val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-
+                val monthOfYear = monthOfYear +1
                 // Display Selected date in textbox
-                dataTesto.setText("" + dayOfMonth + "/" + month + "/" + year)
+                dataTesto.setText("" + dayOfMonth + "/" + monthOfYear + "/" + year)
 
             }, year, month, day)
 
@@ -48,26 +50,26 @@ class PrenotaActivity : AppCompatActivity() {
         val inizioTesto     = findViewById<TextView>(R.id.inizioTesto)
 
         SelezionaInizio.setOnClickListener {
-            val cal = Calendar.getInstance()
+            val cal = getInstance()
             val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
+                cal.set(HOUR_OF_DAY, hour)
+                cal.set(MINUTE, minute)
                 inizioTesto.text = SimpleDateFormat("HH:mm").format(cal.time)
             }
-            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+            TimePickerDialog(this, timeSetListener, cal.get(HOUR_OF_DAY), cal.get(MINUTE), true).show()
         }
 
         val SelezionaFine = findViewById<ImageButton>(R.id.SelezionaFine)
         val fineTesto     = findViewById<TextView>(R.id.fineTesto)
 
         SelezionaFine.setOnClickListener {
-            val cal = Calendar.getInstance()
+            val cal = getInstance()
             val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
+                cal.set(HOUR_OF_DAY, hour)
+                cal.set(MINUTE, minute)
                 fineTesto.text = SimpleDateFormat("HH:mm").format(cal.time)
             }
-            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+            TimePickerDialog(this, timeSetListener, cal.get(HOUR_OF_DAY), cal.get(MINUTE), true).show()
         }
     }
 
